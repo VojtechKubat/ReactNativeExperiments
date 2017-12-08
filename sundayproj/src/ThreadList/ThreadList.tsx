11b842d55aa8dfed9ae89dad731fe1891ffd62fc
Vojtech Kubat <vojtech.kubat.certicon@google.com>
2017-12-08 16:50:51 +0100

    TSLint added; ThreadList.tsx linted

--- sundayproj/src/ThreadList/ThreadList.tsx
+++ sundayproj/src/ThreadList/ThreadList.tsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-  StyleSheet,
-  Text,
-  View,
-  ListView,
-  ListViewDataSource
-} from 'react-native';
+    ListView,
+    ListViewDataSource,
+    StyleSheet,
+    Text,
+    View,
+} from "react-native";
 
-import { Thread } from '../Model/Thread';
+import { Thread } from "../Model/Thread";
 
 const dummyThreads: Thread[] = [
     { title: "First" },
@@ -57,40 +57,52 @@ const dummyThreads: Thread[] = [
     { title: "First" },
     { title: "Second" },
     { title: "Third" },
-    { title: "Fourth" },            
+    { title: "Fourth" },
 ];
 
-export interface MyProps {        
+export interface IMyProps {
+    dummyProp: string;
 }
 
-export interface MyState {
-    title: string
-    dsThreads: ListViewDataSource
+export interface IMyState {
+    title: string;
+    dsThreads: ListViewDataSource;
 }
 
-export default class ThreadList extends Component<MyProps, MyState> {
+export default class ThreadList extends Component<IMyProps, IMyState> {
 
     // state = {
     //     title : "List of threads",
     //     ds: null
     // };
+    public static defaultProps: IMyProps = {
+        dummyProp: "Hello world",
+    }
 
-    constructor (props: MyProps, state: MyState) {
+    constructor (props: IMyProps, state: IMyState) {
         super(props, state);
 
         const ds: ListViewDataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
+            dsThreads: ds.cloneWithRows(dummyThreads),
             title: "All threads",
-            dsThreads: ds.cloneWithRows(dummyThreads)
         };
     }
 
-    static defaultProps: MyProps = {
-        
+    public render() {
+        return (
+            <View style ={styles.container}>
+                <Text style={styles.title}>{this.state.title}</ Text>
+                <ListView
+                    dataSource = {this.state.dsThreads}
+                    renderRow = {this.renderRow.bind(this)}
+                />
+            </View>
+        );
     }
 
-    renderRow(thread: Thread, sectionId: number, highLightRow: number) {
-        console.log('>>> thread: '+thread.title+" - rowNumber: "+sectionId+" - highlight row: "+highLightRow);
+    private renderRow(thread: Thread, sectionId: number, highLightRow: number) {
+        // console.log('>>> thread: '+thread.title+" - rowNumber: "+sectionId+" - highlight row: "+highLightRow);
         return (
             <View style={styles.row}>
                 <Text style={styles.threadLabel}>{thread.title}</Text>
@@ -98,39 +110,28 @@ export default class ThreadList extends Component<MyProps, MyState> {
         );
     }
 
-    render() {
-        return (
-            <View style ={styles.container}>
-                <Text style={styles.title}>{this.state.title}</ Text>
-                <ListView 
-                    dataSource = {this.state.dsThreads}
-                    renderRow = {this.renderRow.bind(this)}
-                />
-            </View>
-        );
-    }
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    },
-    table:{
+        alignItems: "center",
+        backgroundColor: "#F5FCFF",
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    title: {
-        margin: 40,
-        fontSize: 20
+        justifyContent: "center",
     },
     row: {
-        height : 80
+        height : 80,
+    },
+    table:{
+        alignItems: "center",
+        flex: 1,
+        justifyContent: "center",
     },
     threadLabel: {
-        fontSize: 30
-    }
+        fontSize: 30,
+    },
+    title: {
+        fontSize: 20,
+        margin: 40,
+    },
   });
