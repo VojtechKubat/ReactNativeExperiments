b6645024a47567f3a0be390109ffc691ebfdc7d9
Vojtech Kubat <vojtech.kubat.certicon@google.com>
2017-12-08 17:08:50 +0100

    linting2

--- sundayproj/src/App.tsx
+++ sundayproj/src/App.tsx
@@ -4,27 +4,27 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Platform,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
-import Component1 from './components/component1/component1';
-import MyTextfield from './components/myTextField/MyTextfied';
-import ThreadList from './ThreadList/ThreadList'
+  View,
+} from "react-native";
+import Component1 from "./components/component1/component1";
+import MyTextfield from "./components/myTextField/MyTextfied";
+import ThreadList from "./ThreadList/ThreadList"
 // import ButtonComp from './components/buttoncomponent/buttoncomponent';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" +
+    "Cmd+D or shake for dev menu",
+  android: "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu",
 });
 
 export default class App extends Component<{}> {
-  render() {
+  public render() {
     return (
       <View style = {styles.container}>
         <ThreadList dummyProp = "Hello" />
@@ -36,18 +36,18 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
 });
