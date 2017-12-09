5f12c8678ccc229d44929da5e3d105c57a159d9b
Vojtech Kubat <kubat.vojt3ch@gmail.com>
2017-12-09 18:52:57 +0100

    imoapp - style added

--- imoapp/src/Base/Header.js
+++ imoapp/src/Base/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Text } from 'react-native';
+import {  StyleSheet,Text } from 'react-native';
 
-const styles = {
-    test-style: 20
-}
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 40
+    }
+});
 
 const Header = () => {
-    return (<Text>This is header</Text>);
+    return (
+        <Text style={styles.header}>This is header</Text>
+    );
 }
 
 export default Header;
